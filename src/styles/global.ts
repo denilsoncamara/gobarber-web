7da6970435cc7604bfc4a28c207194817713ae06
denilsoncamara <camara.denilson@gmail.com>
2020-07-19 01:44:13 -0300

    First commit: create Login component

---
+++ src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: #312e38;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 1.6rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 500;
+  }
+
+  ul[class],
+  ol[class] {
+    list-style: none
+  }
+`;
