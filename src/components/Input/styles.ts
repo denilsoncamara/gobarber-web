dd93f2792965e8d5b56ba60688d51114f2578c92
denilsoncamara <camara.denilson@gmail.com>
2020-07-19 22:30:43 -0300

    Create Input and Button components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+
+  input {
+    flex: 1;
+    background: transparent;
+    outline: 0;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  > svg {
+    margin-right: 16px;
+  }
+`;
